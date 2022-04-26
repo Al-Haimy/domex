f0160b1704abef538d73e3b6fbf39c1d05ae9426
40D4Y5 <mohammed1alhaimi@gmail.com>
2022-04-26 15:42:44 +0800

    version one is done

--- domex.py
+++ domex.py
@@ -13,23 +13,64 @@ def parse_args():
     parser.add_arhument('')
 
 
-def remove_double():
+def write_file(file_name, domains):
+    with open(file_name, 'w') as file:
+        for domain in domains:
+            file.write(domain+"\n")
+
+
+def remove_double(file_name):
     double = []
     cleanlist = []
-    with open(sys.argv[1], 'r',  encoding='utf-8') as file_domains:
+    with open(file_name, 'r',  encoding='utf-8') as file_domains:
         domains = file_domains.readlines()
         print('Total domains: ' + str(len(domains)))
         for domain in domains:
             if domain not in double:
-                double.append(domain)
+                double.append(domain.strip())
+    return double
+
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', type=str,
+                        required=True, help='target file')
+    parser.add_argument('-e', '--exclude', type=str,
+                        required=False, help='a word to remove from the file')
+    parser.add_argument('-o', '--output', type=str, required=False,
+                        help='if not specified will overrwrite the original')
+    return parser.parse_args()
 
 
 def banner():
-    print(r"""________                                
+    print(r"""________
 \______ \   ____   _____   ____ ___  ___
  |    |  \ /  _ \ /     \_/ __ \\  \/  /
- |    `   (  <_> )  Y Y  \  ___/ >    < 
+ |    `   (  <_> )  Y Y  \  ___/ >    <
 /_______  /\____/|__|_|  /\___  >__/\_ \
         \/             \/     \/      \/""")
     print("Good Luck")
     print("Happy Hacking")
+
+
+def exclude(domains, ex):
+    new_domains = [domain.replace(ex, '') for domain in domains]
+    return new_domains
+
+
+def main():
+    banner()
+    args = parse_args()
+    clean_list = remove_double(args.file)
+    if args.exclude:
+        clean = exclude(clean_list, args.exclude)
+        clean_file = args.output if args.output else args.file
+        write_file(clean_file, clean)
+    else:
+        clean_file = args.output if args.output else args.file
+        write_file(clean_file, clean_list)
+
+
+if __name__ == '__main__':
+    main()
